24e288108a249cb4762b23f0840861b3ea6df448
vilsonmoro <vilsonmoro@gmail.com>
2023-10-26 21:33:45 -0300

    Edição de contatos

--- src/main/java/controllers/ContatoController.java
+++ src/main/java/controllers/ContatoController.java
@@ -12,10 +12,18 @@ public class ContatoController {
     
     public static boolean salvar(Contato ct) {
        try {
-    	   PreparedStatement stm = con.prepareStatement("insert into tb_contatos(nome, email)values(?,?)");
-    	   stm.setString(1, ct.getNome());
-    	   stm.setString(2, ct.getEmail());
-    	   stm.execute();
+    	   PreparedStatement stm;
+    	   if (ct.getId() == 0) {
+    		    stm = con.prepareStatement("insert into tb_contatos(nome, email)values(?,?)");
+    		   stm.setString(1, ct.getNome());
+        	   stm.setString(2, ct.getEmail());        	
+    	   }else {
+    		   stm = con.prepareStatement("update tb_contatos set nome = ?, email = ? where id = ?");
+    		   stm.setString(1, ct.getNome());
+        	   stm.setString(2, ct.getEmail()); 
+        	   stm.setInt(3, ct.getId());
+    	   }
+    	   stm.execute();    	   
        }
        catch(Exception e) {
     	   System.err.println(e.getMessage());
@@ -49,5 +57,20 @@ public class ContatoController {
         return rs;	
      }
 	
+    public static Contato consultar(int id) {
+    	 Contato ct = null;
+        try {
+     	   PreparedStatement stm = con.prepareStatement("select * from tb_contatos where id = ?");
+     	   stm.setInt(1, id);
+     	   ResultSet rs = stm.executeQuery();
+     	   if(rs.next()) {
+     		 ct = new Contato(rs.getInt("id"), rs.getString("nome"), rs.getString("email")); 
+     	   }
+        }
+        catch(Exception e) {
+     	   System.err.println(e.getMessage());     	   
+        }    	
+        return ct;	
+     }
     
 }
